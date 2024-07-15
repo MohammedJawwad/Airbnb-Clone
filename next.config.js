e21fd7bdc7af397a10e0af097ded48f2260de4bc
Mohammed Jawwad <jawwadsyed5786@gmail.com>
2024-07-15 15:30:07 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      "res.cloudinary.com"
+
+    ]
+  }
+}
+
+module.exports = nextConfig
